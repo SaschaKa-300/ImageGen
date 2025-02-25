3a08b930f5f9f2cd4ca4d547c256190f51460d2c
SaschaKa-300 <sascha.kastert@gmail.com>
2025-02-25 22:18:37 +0100

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,13 @@ user_input = st.text_input("Enter your prompt:")
 
 
 # List of different model versions
-model_versions = [
+model_versions = {
     "saschaka-300/matthias-model:9388b390fca72d01d157be9e1e2a4c4ec6664dde9c5b8c115a4432d919a68b79": "Model 1 (Classic)"
     "saschaka-300/matthias1:870e066ebab03a42b5e340a26d7ca2e1a2c6d23aeeb9c7713bc314cc4a423c35": "Model 2"
     "saschaka-300/matthias2:1c0bd0f210fdc4f5377dddf0ad68c7c08f4ac93c5c0635eef09d713f9cd18c0d": "Model 3"
     "saschaka-300/matthias3:c04ea86c96d1118f7adc6fd0fb63a8bad113b8066e52126952c254e4c9f29842": "Model 4"
     "saschaka-300/matthias3morestepsandrank:ea0c55919c10a114b9760662e874e5b8dbdf25d624e6eae2de74b39222564c99": "Model 5 (same images as 4, but longer training)"
-]
+}
 
 
 if st.button("Generate"):
